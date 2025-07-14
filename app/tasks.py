df7b1f51bffa910460851b9de02ec373823a41b2
VenEl101 <acccisco3@gmail.com>
2025-07-14 17:17:54 +0500

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,13 +1,13 @@
 from celery import shared_task
 from django.core.mail import send_mail
-
+from root import settings
 
 @shared_task
 def send_otp_email(email, code):
     print('123')
     subject = "Your Verification Code"
     message = f"Your verification code is: {code}"
-    from_email = 'vnoreply58@gmail.com'
+    from_email = setting.EMAIL_HOST
     recipient_list = [email]
 
     return send_mail(subject, message, from_email, recipient_list)
